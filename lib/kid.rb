c659cbebaf9f5f7fd1c8e4b81db46ddd14ff40a7
Njoki <kinuthiash@gmail.com>
2022-06-13 10:47:01 +0300

    commit tree 2e5bae3660cf839af43d10b7fb3fb39fa9f1e297

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,23 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative "./fancy_dance"
+
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  include Dance
+  extend MetaDancing
+
+  attr_accessor :name
+
+  def initialize name
+    @name = name
+  end
+end
+
+james = Kid.new "James"
+puts james.twirl
+
+puts Kid.metadata
